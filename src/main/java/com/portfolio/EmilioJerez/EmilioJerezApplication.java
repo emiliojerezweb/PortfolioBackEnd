c3af6b7c3acac135c5fd0154e3c3e78ae05cd8a8
Milo <jerezemilio@hotmail.com>
2022-08-01 12:17:35 -0300

    Inicio

---
+++ src/main/java/com/portfolio/EmilioJerez/EmilioJerezApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.EmilioJerez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmilioJerezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmilioJerezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/EmilioJerez/EmilioJerezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.EmilioJerez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmilioJerezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
